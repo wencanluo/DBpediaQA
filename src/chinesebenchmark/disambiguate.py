12737e48c7b9f4010d7776d36ede09d4b4df2b20
wencanluo <wencanluo.cn@gmail.com>
2014-06-23 10:01:45 -0400

    Diambiguation result for the chinese

--- src/chinesebenchmark/disambiguate.py
+++ src/chinesebenchmark/disambiguate.py
@@ -2,6 +2,10 @@
 
 #!/usr/bin/python
 import httplib, urllib
+import sys, json, codecs
+import chinesebenchmark
+import fio
+import slpSRL
 
 def getNEL(text = ""):
     # get disambiguation result using Agistis
@@ -11,15 +15,37 @@ def getNEL(text = ""):
     conn = httplib.HTTPConnection("127.0.0.1:8080")
     conn.request("POST", "/AGDISTIS", params, headers)
     response = conn.getresponse()
-    print response.status, response.reason
-    data = response.read()
-    print data
-    conn.close()
-
-if __name__ == '__main__':
-    #input = '../../benchmark/qald4/qald-4_multilingual_train_withanswers_zh.xml'
-    #output = '../../benchmark/qald4/qald-4_multilingual_train_withanswers_zh_srl.json'
     
-    input = '../../benchmark/qald4/qald-4_multilingual_test_questions_zh.xml'
-    output = '../../benchmark/qald4/qald-4_multilingual_test_questions_zh_srl.json'
-    getNEL('我 是 <entity>中国</entity> 人')
+    try:
+        data = response.read()
+    except Exception:
+        data = None
+        print response.status, response.reason
+    conn.close()
+    return data
+
+def getDisambiguate(input, output):
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    
+    lines = fio.readfile(input)
+    
+    SavedStdOut = sys.stdout
+    sys.stdout = codecs.open(output, 'wb', 'utf8')
+    
+    for line in lines:
+        line = line.rstrip()
+        s = getNEL(line)
+        print s
+            
+    sys.stdout = SavedStdOut
+    
+if __name__ == '__main__':
+    #input = '../../benchmark/qald4/qald-4_multilingual_train_withanswers_ner.txt'
+    #output = '../../benchmark/qald4/qald-4_multilingual_train_withanswers_zh_disambiguation.txt'
+    
+    input = '../../benchmark/qald4/qald-4_multilingual_test_questions_zh_ner.txt'
+    output = '../../benchmark/qald4/qald-4_multilingual_test_questions_zh_disambigution.txt'
+    #getNEL('我 是 <entity>中国</entity> 人')
+    
+    getDisambiguate(input, output)
